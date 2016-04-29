c721b7b33c31e811674ef3f085800cf533753998
Mario <mario.c.mendes1985@gmail.com>
2016-04-30 00:03:55 +0100

    adds main.js and check-access-token.js

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
  */
 'use strict';
 
+function HipChatLogger(roomName) {
+  var main = require('./lib/modules/main');
+  var self = this;
+
+  main.run('./config.json', function (err) {
+    if (err) {
+      console.log('There was an error setting up the configs: ' + err);
+      return;
+    }
+    // do some cool stuff here - define functions to log info and set room
+  });
+
+}
+
+module.export = HipChatLogger;
+
+
+
